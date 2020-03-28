e0b4c8aea13d527b511175ffedbe40e339a6a662
ntsd <jo06942@gmail.com>
2020-03-29 05:32:22 +0700

    refactor: .gitignore

--- lib/src/MaintenancePage.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-export interface MaintenancePageProps extends React.Props<MaintenancePage> {
-    color: string;
-}
-export declare class MaintenancePage extends React.Component<MaintenancePageProps, any> {
-    render(): JSX.Element;
-}
