1370b0eabe250f8f4dcf7c933772fa901a197386
ntsd <jo06942@gmail.com>
2020-05-03 15:08:45 +0700

    feat: add svgrrc to config svgr

--- src/maintenance-page.tsx
+++ src/maintenance-page.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Constructions } from './assets/constructions';
+import { SvgConstructions } from './assets/constructions';
 
 export interface MaintenancePageProps {
   color: string;
@@ -22,7 +22,7 @@ export class MaintenancePage extends React.Component<
 
   render() {
     return (
-      <Constructions
+      <SvgConstructions
         style={{
           height: '100%',
           width: '100%',
@@ -44,7 +44,7 @@ export class MaintenancePage extends React.Component<
         >
           {this.props.children}
         </div>
-      </Constructions>
+      </SvgConstructions>
     );
   }
 }
